50d803df2b095b699295ba4f08609ff0156d1841
realNoddy <real_noddy@yahoo.com>
2023-07-04 01:40:15 +0200

    allow http requests

--- Dashboard/Dashboard/wnetwrap.cpp
+++ Dashboard/Dashboard/wnetwrap.cpp
@@ -93,8 +93,20 @@ internetopened: //used for absolute URI redirect where both path and host have c
 		return output;
 	}
 
-connectionopened: //used for relative URI redirect where only path (not host) changes
-	HINTERNET hRequest = HttpOpenRequestA(hConnect, request.Method.c_str(), URI["path"].c_str(), NULL, NULL, NULL, INTERNET_FLAG_SECURE | INTERNET_FLAG_NO_AUTO_REDIRECT, 0);
+	connectionopened: //used for relative URI redirect where only path (not host) changes
+
+	// Set flags based on request protocol
+	DWORD dwFlags = 0;
+	if (URI["scheme"] == "https")
+	{
+		dwFlags = INTERNET_FLAG_SECURE | INTERNET_FLAG_NO_AUTO_REDIRECT;
+	}
+	else if (URI["scheme"] == "http")
+	{
+		dwFlags = INTERNET_FLAG_NO_AUTO_REDIRECT;
+	}
+
+	HINTERNET hRequest = HttpOpenRequestA(hConnect, request.Method.c_str(), URI["path"].c_str(), NULL, NULL, NULL, dwFlags, 0);
 
 	if (hRequest == NULL)
 	{
